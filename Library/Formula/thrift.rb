f07766a7db7bc09db798cbec4bc273895965b09c
Max Hansmire <max@bluefinlabs.com>
2012-08-28 10:27:29 -0400

    revert to the thrift version 0.6.1 and fix the URL for that release

--- Library/Formula/thrift.rb
+++ Library/Formula/thrift.rb
@@ -1,11 +1,10 @@
 require 'formula'
 
 class Thrift < Formula
-  homepage 'http://thrift.apache.org'
-  url 'http://www.apache.org/dyn/closer.cgi?path=thrift/0.8.0/thrift-0.8.0.tar.gz'
-  md5 'd29dfcd38d476cbc420b6f4d80ab966c'
-
+  homepage 'http://incubator.apache.org/thrift/'
   head 'http://svn.apache.org/repos/asf/thrift/trunk'
+  url 'http://archive.apache.org/dist/thrift/0.6.1/thrift-0.6.1.tar.gz'
+  md5 'e1ec722d5f38077a23a32c4de4d4ce94'
 
   depends_on 'boost'
 
@@ -15,7 +14,7 @@ class Thrift < Formula
     # isn't needed anymore at all but I can't test that.
     cp "/usr/X11/share/aclocal/pkg.m4", "aclocal" if MACOS_VERSION < 10.7
 
-    system "./bootstrap.sh" if ARGV.build_head?
+    system "./bootstrap.sh" if version == 'HEAD'
 
     # Language bindings try to install outside of Homebrew's prefix, so
     # omit them here. For ruby you can install the gem, and for Python
@@ -28,22 +27,19 @@ class Thrift < Formula
                           "--without-python",
                           "--without-ruby",
                           "--without-perl",
-                          "--without-php",
-                          "--without-erlang"
+                          "--without-php"
     ENV.j1
     system "make"
     system "make install"
   end
 
   def caveats; <<-EOS.undent
-    Most language bindings were not installed. You may like to do the
-    following:
+    Some bindings were not installed. You may like to do the following:
 
         gem install thrift
         easy_install thrift
 
-    If anyone figures out the steps to reliably build a set of bindings, please
-    open a pull request.
+    Perl and PHP bindings are a mystery someone should solve.
     EOS
   end
 end
